b1c448a859c6432e5a4c6f7cc48d1bf81d07f9fb
Sydney Blackburn <sydneyblackburn10@gmail.com>
2018-11-14 15:37:29 -0800

    Initial Commit

---
+++ Assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by Sydney Blackburn on 11/14/18.
+//  Copyright © 2018 Sydney Blackburn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Assignment5Tests/Assignment5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment5Tests.swift
+//  Assignment5Tests
+//
+//  Created by Sydney Blackburn on 11/14/18.
+//  Copyright © 2018 Sydney Blackburn. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment5
+
+class Assignment5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
